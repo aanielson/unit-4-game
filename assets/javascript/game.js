029d00a08261e8ccc1488fdad09faa506f99f5ad
Abigail Nielson <aa.nielson18@gmail.com>
2020-03-21 12:11:54 -0700

    pseudo coded basic structure of js

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+//display a random number between 19-120 inside of the id goal
+
+//assign each crystal a random number between 1-12
+    //ruby
+    //diamond
+    //opal
+    //emerald
+
+//when one crystal is clicked, add that assigned amount to the id/variable score
+    //ruby
+    //diamond
+    //opal
+    //emerald
+
+//if the score matches the beginning random number, increase wins id/var
+    //reset and randomize goal & crystal numbers
+
+//if the score goes over the beginning random number, increase losses id/var
+    //reset and randomize goal & crystal numbers
+
+
